066cca27e3a8a578afdac2a091b79a79b9fa3297
mthiaw <massaw6@gmail.com>
2024-02-05 11:29:08 +0000

    uodate: broadcasr func corrected

--- socket-side/websocketTools.go
+++ socket-side/websocketTools.go
@@ -1,16 +1,38 @@
 package Socket
 
-import "log"
+import (
+	"fmt"
+	"log"
+)
 
 func (c *SocketReader) Broadcast(message map[string]interface{}) {
-	log.Println("broadcasting...📡")
-	for _, socket := range UserTab {
+	log.Println("broadcasting to...📡")
+	for i := range UserTab {
+		fmt.Printf("%d => %v\n", i, UserTab[i])
+	}
 
+	for _, socket := range UserTab {
+		fmt.Println("user found =>", socket, socket.Connected)
+		if !socket.Connected {
+			// no send message to offline users
+			continue
+		}
+		socket.SendMessage(message)
+	}
+}
+
+func (c *SocketReader) NotifyOthers(message map[string]interface{}) {
+	log.Println("broadcasting to...📡")
+	for i := range UserTab {
+		fmt.Printf("%d => %v\n", i, UserTab[i])
+	}
+
+	for _, socket := range UserTab {
+		fmt.Println("user found =>", socket, socket.Connected)
 		if socket == c {
 			// no send message to himself
 			continue
 		}
-
 		if !socket.Connected {
 			// no send message to offline users
 			continue
