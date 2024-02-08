97c9e8887bfa76c9ad291c00932e77746313e8d0
mthiaw <massaw6@gmail.com>
2024-02-08 20:44:37 +0000

    update: error comment display script + style added

--- templates/error/alert.mjs
+++ templates/error/alert.mjs
@@ -1,13 +1,27 @@
 export const alertError = (props = {}) => {
   console.log("in alert");
-  const field = document.getElementsByTagName("form")[0];
   const err = document.createElement("p");
-  err.id = `error-form-${props.Location}`;
-  err.innerHTML = props.Msg;
-  if (!document.getElementById("error-form")) {
-    field.appendChild(err);
+  if (props.Location != "homeComment") {
+    const field = document.getElementsByTagName("form")[0];
+    err.id = `error-form-${props.Location}`;
+    err.innerHTML = props.Msg;
+    if (!document.getElementById("error-form")) {
+      field.appendChild(err);
+    }
+    setTimeout(() => {
+      field.removeChild(err);
+    }, 2000);
+  } else {
+    console.log("alert comment");
+    const post = document.getElementById(props.Post),
+      commentZone = post.querySelector(`.allaboutcomment`),
+      commentForm = commentZone.querySelector(".new-comment-form");
+    const submitButton = commentForm.querySelector("button");
+    err.id = "error-comment";
+    err.innerHTML = props.Msg;
+    commentForm.insertBefore(err, submitButton);
+    setTimeout(() => {
+      commentForm.removeChild(err);
+    }, 2000);
   }
-  setTimeout(() => {
-    field.removeChild(err);
-  }, 2000);
 };
